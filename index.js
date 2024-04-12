9e412589ed8c285d8a5707533c0cbf3909f0d9b7
陈刑 <amaliegay@outlook.com>
2024-04-12 15:30:19 +0800

    adding more debug log

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ const getDate = (unix) => {
 const novelChartUrlBase = "https://www.jjwxc.net/channeltoplist.php?rchannelid=109&rankgroup=3&subchannelid=";
 
 for (const genre in genres) {
+    console.log(`processing genre ${genre}`);
     const novelChartUrl = `${novelChartUrlBase}${genres[genre]}`;
     getNovelUrls(novelChartUrl)
         .then((novelUrls) => {
